fcfa39d7ce3f7d4f141deb7b4ee31a9b01a1956f
MBA2022 <mohdbm.amr@gmail.com>
2023-08-27 16:07:13 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+const mongoose = require('mongoose');
+const eventHandler = require('./handlers/eventHandler');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildPresences,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+
+(async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to DB.');
+
+    eventHandler(client);
+
+    client.login(process.env.TOKEN);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+})();
+
+let status = [
+  {
+    name: 'Neat Game',
+    type: ActivityType.Playing,
+  },
+];
+
+client.on('ready', (c) => {
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[random]);
+  }, 10000);
+});
